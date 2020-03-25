9833f86bb4ce63dc41e789c02b2883cd34847760
Ashu Ghildiyal <ashu@kinvolk.io>
2020-03-25 18:27:47 +0530

    undestanding slices,make and new

--- main.go
+++ main.go
@@ -5,14 +5,24 @@ import (
 
 	sample "github.com/ashu8912/go_learning/pkg/deferbehaviour"
 )
-func evaluateSomething(arg string) string{
-   fmt.Println("Evaluating something")
+
+func evaluateSomething(arg string) string {
+	fmt.Println("Evaluating something")
 	return arg
 }
-func main(){
- for i:=0;i<5;i++{
-	 defer fmt.Println(i)
- }
+func main() {
+	a:=[...]int{1,2,3,4,5}
+	sl:=a[:]
+	sl=append(sl,6,7,8,9)
+	b:=make([]int,4)
+	for i:=0;i<len(b);i++{
+		b[i]=i*5
+	}
+	sl=append(sl,b...)
+	fmt.Println(sl)
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
 
- defer sample.ShowDeferBehaviour(evaluateSomething(("ashu ghildiyal"))
+	defer sample.ShowDeferBehaviour(evaluateSomething(("ashu ghildiyal")))
 }
--- pkg/deferbehaviour/deferbehaviour.go
+++ pkg/deferbehaviour/deferbehaviour.go
@@ -3,7 +3,7 @@ package deferbehaviour
 import "fmt"
 
 //this funtion is a helper function to show defer behaviour
-func ShowDeferBehaviour(arg string) string{
+ func ShowDeferBehaviour(arg string) string{
  fmt.Println(" Hello ",arg," you are in a defered function")
  return arg
 }
